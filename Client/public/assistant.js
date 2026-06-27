42a76bd4addde1bdf598267c4e9c66c7bdcff1a0
shivampaliwal244-ops <shivampaliwal244@gmail.com>
2026-06-27 23:09:50 +0530

    minor bug fix

--- Client/public/assistant.js
+++ Client/public/assistant.js
@@ -18,7 +18,7 @@
 
     link.rel = "stylesheet"
 
-    link.href = "http://localhost:5173/assistant.css"
+    link.href = "https://ai-voice-assistant-y7a8.onrender.com/assistant.css"
 
     document.head.appendChild(link)
 
@@ -83,7 +83,7 @@
             <button class="shifra-mic">
 
                <img 
-               src="http://localhost:5173/mic.svg"
+               src="https://ai-voice-assistant-y7a8.onrender.com/mic.svg"
                alt="mic"
                class="shifra-mic-icon"/>
             </button>
@@ -102,7 +102,7 @@
 
     button.innerHTML = `
     <img 
-    src="http://localhost:5173/logo.png"
+    src="https://ai-voice-assistant-y7a8.onrender.com/logo.png"
     alt="logo"
     />`;
     document.body.appendChild(button)
