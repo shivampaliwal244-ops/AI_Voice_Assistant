fece3aa275dfb9f027c96e78d68aeb7b884b9a64
shivampaliwal244-ops <shivampaliwal244@gmail.com>
2026-06-27 23:11:13 +0530

    client url add

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,7 +9,7 @@ import Builder from './pages/Builder'
 import Billing from './pages/Billing'
 import { Toaster } from "react-hot-toast"
 export const ServerUrl = "https://ai-voice-assistant-server-backend.onrender.com"
-export const CLIENT_URL = "http://localhost:5173"
+export const CLIENT_URL = "https://ai-voice-assistant-y7a8.onrender.com"
 function App() {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
